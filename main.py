6e5f3c61824c8ff20f4582749dc8145fd7b25a64
Ethan Bielecki <ebielecki2003@gmail.com>
2023-08-25 22:18:42 -0400

    Cleaned up output of weather information

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import json
 import requests
 
-key = input('Enter your key from weatherapi.com: ')
+# weatherapi.com key can go below, between the '' otherwise, it will ask the user for a key
+key = ''
+
+if key == '':
+    key = input('Enter your key from weatherapi.com: ')
 
 location = input('Enter 5 digit zip code: ')
 chooseForecast = int(input('1 Day Forecast or 3 Day Forecast (Enter 1 or 3): '))
@@ -27,16 +31,28 @@ print('Country:    ' + data['location']['country'])
 print('Timezone:   ' + data['location']['tz_id'])
 print('Local time: ' + data['location']['localtime'])
 print('\n-=-=-=-=-=-=-=-Current Weather-=-=-=-=-=-=-=-=-=-\n')
-print('Temperature: ' + str(currentWeather['temp_f']) + ' F (' + str(currentWeather['temp_c']) + ')')
-print('Feels like:  ' + str(currentWeather['feelslike_f']) + ' F (' + str(currentWeather['feelslike_c']) + ')')
+print('Temperature: ' + str(currentWeather['temp_f']) + ' F (' + str(currentWeather['temp_c']) + ' C)')
+print('Feels like:  ' + str(currentWeather['feelslike_f']) + ' F (' + str(currentWeather['feelslike_c']) + ' C)')
 print('Humidity:    ' + str(currentWeather['humidity']))
 print('-=-=-=-=-=-=-=-Hour by Hour Weather-=-=-=-=-=-=-=-=-=-\n')
 
 tableLength = 23
 
 for i in forecastDay:
-    print((' ' * int((tableLength / 2 - len(i['date']) / 2)) + i['date']))
-    print('hours | temp_f | temp_c')
-    print('-' * 23)
-    for j in i['hour']:
-        print(j['time'][-5:] + ' |  ' + str(j['temp_f']) + '  |  ' + str(j['temp_c']))
+    print((' ' * int((tableLength / 2 - len(i['date']) / 2)) + i['date']) +
+          (' ' * int((tableLength / 2 - len(i['date']) / 2) + 10)), end="")
+
+print()
+
+for i in range(0, 3):
+    print('hours | temp_f | temp_c' + ' ' * 9, end="")
+
+print()
+print('-' * 23 + ' ' * 9 + '-' * 23 + ' ' * 9 + '-' * 23)
+
+
+for j in range(0, 24):
+    for i in forecastDay:
+        print(i['hour'][j]['time'][-5:] + ' |  ' + str(i['hour'][j]['temp_f']) + '  |  ' +
+              str(i['hour'][j]['temp_c']) + ' ' * 10, end="")
+    print()
